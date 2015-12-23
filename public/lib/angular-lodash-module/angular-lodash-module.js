64c1652ea9cccf91b2c18f0fbc5ce1e5a294697a
Anthony Pasquale <apasquale@anzgcis.com>
2015-12-23 11:40:25 +1100

    untracked bower modules

--- public/lib/angular-lodash-module/angular-lodash-module.js
+++
@@ -1,7 +0,0 @@
-/**
- * Let's you use lodash as a service from a controller.
- * Adapted from https://github.com/andresesfm/angular-underscore-module
- * Homepage https://github.com/kmc059000
- */
- // assumes lodash has already been loaded on the page
-angular.module('lodash', []).constant('_', window._);
